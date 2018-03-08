f503bce68eef6c7c29b3c1e0144928f410f989f1
Davin Hazard <davinhazard@gmail.com>
2018-03-08 16:45:17 -0500

    implemented tabbed portfolio sections

--- resources/js/openTab.js
+++ resources/js/openTab.js
@@ -1,13 +1,25 @@
 function openTab(evt, tabName) {
 	var i, tabcontent, tablinks;
-	/*tabcontent = document.getElementByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}*/
+	tabcontent = document.getElementsByClassName("tabcontent");
 	tablinks = document.getElementsByClassName("tablinks");
 	for (i = 0; i < tablinks.length; i ++) {
 		tablinks[i].className = tablinks[i].className.replace(" selected", "");
 	}
-	//document.getElementById(tabName).style.display = "block";
-	evt.currentTarget.className += " selected";
+	/* if button clicked is 'View All Work', 
+	set all tabcontent elements to inline-block */
+	if (tabName === 'view-all') {
+		for (i = 0; i < tabcontent.length; i++) {
+			tabcontent[i].style.display = "inline-block";
+		}
+	}
+
+	/* else, set each tabcontent element to display:none
+	and set the clicked tab to selected and display:block */
+	else {
+		for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+		}
+		document.getElementById(tabName).style.display = "block";
+		evt.currentTarget.className += " selected";
+	}
 }

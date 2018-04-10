843518e70cc351d359ff2e27699fe9f36b4b39b1
Davin Hazard <davinhazard@gmail.com>
2018-04-09 22:05:42 -0400

    added content and fixed mobile font sizing

--- resources/js/animateOnScroll.js
+++ resources/js/animateOnScroll.js
@@ -1,20 +1,26 @@
 $(window).scroll(function () {
-	$('.overlay').each(function () {
+	$(".overlay").each(function () {
 	var imagePos = $(this).offset().top;
 	var imageHeight = $(this).height();
 	var topOfWindow = $(window).scrollTop();
+	var bottomOfWindow = topOfWindow + $(window).height();
+	var headerHeight = $("#header").outerHeight();
 
-	if (imagePos < topOfWindow + imageHeight 
+	/*if (imagePos < topOfWindow + imageHeight 
 		&& imagePos + imageHeight > topOfWindow
-		&& imagePos > topOfWindow) {
-			$(this).addClass("animate-on-scroll");
-			$(this).next(".title").addClass("show-title");
-			$(this).parent().prev(".preview-image").addClass("show-gif");
+		&& imagePos > topOfWindow) { */
+	/* show gif/overlay/title when item is in viewport */
+	if (imagePos + imageHeight < bottomOfWindow
+		&& imagePos > topOfWindow + headerHeight) 
+	{
+		$(this).addClass("animate-on-scroll");
+		$(this).next(".title").addClass("show-title");
+		$(this).parent().prev(".preview-image").addClass("show-gif");
 	}
 	else {
-			$(this).removeClass("animate-on-scroll");
-			$(this).next(".title").removeClass("show-title");
-			$(this).parent().prev(".preview-image").removeClass("show-gif");
+		$(this).removeClass("animate-on-scroll");
+		$(this).next(".title").removeClass("show-title");
+		$(this).parent().prev(".preview-image").removeClass("show-gif");
 	}
 	});
 });
